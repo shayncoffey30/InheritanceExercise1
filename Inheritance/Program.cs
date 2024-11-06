929ac3790199bf9d480a510f56cd021e14af5f0f
Shayn Coffey <Shayncoffey30@gmail.com>
2024-11-05 22:56:26 -0500

    completed

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace Inheritance
 {
@@ -6,34 +7,56 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-            // TODO Be sure to follow best practice when creating your classes
-
-            // Create a class Animal
-            // give this class 4 members that all Animals have in common
-
-
-            // Create a class Bird
-            // give this class 4 members that are specific to Bird
-            // Set this class to inherit from your Animal Class
-
-            // Create a class Reptile
-            // give this class 4 members that are specific to Reptile
-            // Set this class to inherit from your Animal Class
-
-
-
 
             /*Create an object of your Bird class
-             *  give values to your members using the object of your Bird class
-             *  
-             * Creatively display the class member values 
+             * give values to your members using the object ofm your Bird class
+             * 
+             * Creatively display the class member values
              */
 
-            /*Create an object of your Reptile class
-             *  give values to your members using the object of your Reptile class
-             *  
-             * Creatively display the class member values 
-             */
+            var myBird = new Bird();
+            myBird.WingColor = "blue";
+            myBird.CanFly = true;
+            myBird.WillMigrate = true;
+            myBird.BeakLength = 3.5;
+
+            var lizard = new Reptile()
+            {
+                IsColdBlooded = true,
+                IsScaly = true,
+                Habitat = "swamp",
+                CanGrowTail = true
+
+            };
+
+            var myAnimals = new Animal[] { myBird, lizard };
+
+            foreach(var animal in myAnimals)
+            {
+                Console.WriteLine($"Alive:{animal.IsAlive}");
+                Console.WriteLine($"Age:{animal.Age} years old");
+                Console.WriteLine($"It has{animal.LegCount} legs");
+                Console.WriteLine($"It lives by {animal.LandSeaAir}");
+                Console.WriteLine($"");
+            }
         }
+
     }
+
 }
+
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Inheritance/Reptile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Inheritance
+{
+    public class Reptile : Animal
+    {
+        public Reptile()
+        {
+            IsAlive = true; 
+            LegCount = 4;
+            LandSeaAir = "Air";
+            Age = 0;
+        }
+
+        public bool IsColdBlooded { get; set; }
+        public bool IsScaly { get; set; }
+        public string Habitat { get; set; }
+        public bool CanGrowTail { get; set; }
+
+    }
+
+
+
+    
+
+
+
+
+
+
+
+
+}
